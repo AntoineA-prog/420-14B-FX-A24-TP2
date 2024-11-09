5a67b49bf98e2d6e0a4ddd62851754a59e8e9802
Poncho568 <4gamingonly115@gmail.com>
2024-11-09 01:24:17 -0500

    Resterais CalculerTempsCourseMoyen et tester. 30-45 min max et ensuite je passe au reste de prévu

--- 420-14B-FX-A24-TP2/classes/Course.cs
+++ 420-14B-FX-A24-TP2/classes/Course.cs
@@ -297,9 +297,9 @@ namespace _420_14B_FX_A24_TP2.classes
                 Coureurs.Add(coureur);
                 //À changer
 
-            }        
+            }
             //Switch?
-            Coureurs.Sort();
+            TrierCoureurs();
             //good sort?
         }
 
@@ -331,32 +331,82 @@ namespace _420_14B_FX_A24_TP2.classes
             //À implémenter dans la fonction AjouterCoureur.
         }
 
-        public override bool Equals(object? obj)
-        {
-
-        }
-
+        /// <summary>
+        /// Permet de calculer le temps moyen de la course.Les coureurs ayant abandonné la course sont exclus du calcul
+        /// </summary>
         private TimeSpan CalculerTempsCourseMoyen()
         {
 
         }
+        /// <summary>
+        ///Permet de retirer un coureur de la liste de coureurs
+        /// </summary>
+        /// <param name="coureur"></param>
         public void SupprimerCoureur(Coureur coureur)
         {
+            foreach (Coureur coureur in Coureurs)
+            {
 
+                //ok
+                if (coureur == Coureurs.[i])
+                {
+                    Coureurs.RemoveAt(i);
+
+                }
+
+            }
         }
-
+        /// <summary>
+        /// Permet de trier la liste des coureurs selon le temps de course en ajustant le rang des coureurs.
+        /// </summary>
         public void TrierCoureurs()
         {
-
+            Coureurs.Sort();
         }
-        
+
+        /// <summary>
+        /// Permet de comparer deux courses
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj is not Course) return false;
+
+
+            return this == (Course)obj;
+        }
+
+        /// <summary>
+        ///^Permet de comparer deux courses
+        /// </summary>
+        /// <param name="courseGauche"></param>
+        /// <param name="courseDroite"></param>
+        /// <returns></returns>
         public static bool operator ==(Course courseGauche, Course courseDroite)
         {
-            
+            if (object.ReferenceEquals courseGauche, courseDroite))
+                return true;
+
+            if (courseGauche == null || courseDroite == null)
+                return false;
+
+            if (courseGauche.Equals(courseDroite))
+            {
+                return true;
+            }
+
+            return false;
         }
+        /// <summary>
+        /// Permet de comparer deux courses
+        /// </summary>
+        /// <param name="courseGauche"></param>
+        /// <param name="courseDroite"></param>
+        /// <returns></returns>
         public static bool operator !=(Course courseGauche, Course courseDroite)
         {
-            
+            return (courseGauche != courseDroite);
         }
 
         /// <summary>
