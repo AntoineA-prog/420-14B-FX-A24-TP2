6e159e46de51bb6d9cfe00efc56d7a8a00ee46fc
tomasrock123 <tomas@renaudmejia.com>
2024-11-11 13:36:21 -0500

    debut de charger Course

--- 420-14B-FX-A24-TP2/classes/GestionCourse.cs
+++ 420-14B-FX-A24-TP2/classes/GestionCourse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _420_14B_FX_A24_TP2.enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security;
@@ -12,14 +13,14 @@ namespace _420_14B_FX_A24_TP2.classes
         /// <summary>
         /// Les courses
         /// </summary>
-		private Course _courses;
+		private List<Course> _courses;
 
 
         /// <summary>
         /// Obtien ou modifie les courses.
         /// </summary>
-        public Course Courses
-		{
+        public List<Course> Courses
+        {
 			get { return _courses; }
 			set { _courses = value; }
 		}
@@ -31,16 +32,43 @@ namespace _420_14B_FX_A24_TP2.classes
         /// <param name="cheminFichierCoureurs_">Chemin au fichier avec les coureurs</param>
         public GestionCourse(string cheminFichierCourses, string cheminFichierCoureurs_)
         {
-           
+           Courses =new List<Course>();
         }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="cheminFichierCourses"></param>
         /// <param name="cheminFichierCoureurs"></param>
         private void ChargerCourse(string cheminFichierCourses, string cheminFichierCoureurs)
         {
+            if(string.IsNullOrWhiteSpace(cheminFichierCoureurs))
+               throw new ArgumentNullException("Error", "Le chemin pour le fichier coureurs est vide ");
+
+            if(string.IsNullOrWhiteSpace(cheminFichierCourses))
+                throw new ArgumentNullException("Error", "Le chemin pour le fichier course est vide ");
+
+            string[] vectDonnee = Utilitaire.ChargerDonnees(cheminFichierCourses,);
+            string[] vectLigne;
+
+
+            for (int i = 0; i <= vectDonnee.Length - 2; i++)
+            {
+                vectLigne = vectDonnee[i + 1].Split(';');
+                Guid id = Guid.Parse(vectLigne[0]);
+                string nom = vectLigne[1];
+                string ville = vectLigne[2];
+                Province province = (Province)(vectLigne[3]);
+                DateOnly date = DateOnly.Parse(vectLigne[4]);
+                TypeCourse type = (TypeCourse)(vectLigne[5]);
+                ushort distance = ushort.Parse(vectLigne[6]);
+
+
+                Course course=new Course(id, nom, ville, province, date, type,distance);
+
+            }
+
+
 
         }
 
